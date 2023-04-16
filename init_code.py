4848ffa5c45d8098fa2b76c09eee89bae5778d30
stormhun <80296627+burro-jockey@users.noreply.github.com>
2023-04-16 13:21:15 -0700

    get rid of code init code

--- init_code.py
+++ init_code.py
@@ -1,8 +1,1 @@
-import RPi.GPIO as GPIO
-from time import time
-from ob import IRDetector
 
-detcor = IRDetector([11], [13, 15])
-detcor.run_for_duration(3o)
-detcor.cleanup()
-print("Thanks for running!")
---
+++ main.py
@@ -0,0 +1,25 @@
+
+import RPi.GPIO as GPIO
+from time import time
+from ob import IRDetector
+
+def main(seconds = 30): 
+    # do stuff
+    
+    detcor = IRDetector([11], [13, 15])
+    detcor.run_for_duration(seconds)
+    detcor.cleanup()
+    print("Thanks for running!")
+
+import argparse
+
+
+if __name__ == '__main__':
+    # Set up argparse to parse command line arguments
+    parser = argparse.ArgumentParser(description='Wait for a number of seconds.')
+    parser.add_argument('seconds', metavar='N', type=int,
+                        help='the number of seconds to wait')
+    args = parser.parse_args()
+
+    # Call the main function with the number of seconds
+    main(args.seconds)
