a9e828c52f63d81a3d68af08dd47d6e334d18daf
stormhun <80296627+burro-jockey@users.noreply.github.com>
2023-04-16 13:17:28 -0700

    fix main

--- init_code.py
+++ init_code.py
@@ -1,23 +1,8 @@
 import RPi.GPIO as GPIO
 from time import time
+from ob import IRDetector
 
-# Numbers GPIOs by physical location
-GPIO.setmode(GPIO.BOARD)
-# set pin 11 as an input pin with default as LOW v
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# define a function to acquire data
-def binary_acquire(pin, duration):
-    # acquires data as quickly as possible
-    t0 = time() # time is in seconds here
-    results = []
-    while (time() - t0) < duration:
-        results.append(GPIO.input(pin))
-    return results
-
-print("Acquiring data for 1 second")
-# acquire data for 1 second
-results = binary_acquire(11, 1.0)
-print("Done!")
-print(",".join([str(result) for result in results])
-GPIO.cleanup()
+detcor = IRDetector([11], [13, 15])
+detcor.run_for_duration(3o)
+detcor.cleanup()
+print("Thanks for running!")
